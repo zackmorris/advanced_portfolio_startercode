b25caf5f737f2c07950d26be2279af4a7d608f48
Peter Jablonski <pwjablonski@gmail.com>
2017-08-01 13:53:11 -0400

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Unit 4 Portfolio with Object
+// 1.1) 
+    // introduction to data
+// 1.2) 
+    // use JavaScript to create a simple object
+// 2.1) 
+    // create a data representation of your portfolio using nested objects
+// 2.2) 
+    // learn to access properties of an object using dot and bracket notation
+    // use jquery to update your portfolio html
+// 3.1) 
+    // add new properties, modify, and delete properties an object
+    // add like buttons to your html and like property to your object
+//3.2) 
+    // update existing properties of an object
+    // add a click handler to update the like counter when buton is clicked
