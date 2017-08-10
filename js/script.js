94d83fa660d89367c3929eea5e6ee3042c58aae1
Peter Jablonski <pwjablonski@gmail.com>
2017-08-10 10:51:11 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 //Unit 4 Portfolio with Object
-// 1.1) 
-    // introduction to data
-// 1.2) 
-    // use JavaScript to create a simple object
-// 2.1) 
+// Class 1 
+    // use JavaScript to create a simple object representation of your portfolio
+// Class 2
     // create a data representation of your portfolio using nested objects
-// 2.2) 
     // learn to access properties of an object using dot and bracket notation
-    // use jquery to update your portfolio html
-// 3.1) 
-    // add new properties, modify, and delete properties an object
+    // use jquery to update your portfolio html (make sure to add appropriate class to your HTML
+// Class 3
     // add like buttons to your html and like property to your object
-//3.2) 
+    // add new properties, modify, and delete properties an object
     // update existing properties of an object
     // add a click handler to update the like counter when buton is clicked
+
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+
+
+// Create your potrfolio object representation below
+
+
+
+
+
+
+
+// Use Jquery to update your HTML using your object. Don't forget the Document Ready function. 
+
+
+
+
+
+
+
+// Add your click handler below to update your like counters when the like buttons are clicked. 
